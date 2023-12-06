598f67dbebecf4ca22b27c52eb8171209a690296
xujing <xujing@shouqianba.com>
2023-12-06 17:58:38 +0800

    push

--- src/App.tsx
+++ src/App.tsx
@@ -20,7 +20,7 @@ function App() {
       <div className="card">
         <button onClick={() => setCount(count => count + 1)}>count is {count}</button>
         <p>
-          Ediwtttt <code>src/App.tsx</code> and save to test HMR
+          Ediwtttt12 <code>src/App.tsx</code> and save to test HMR
         </p>
       </div>
       <p className="read-the-docs">Click on the Vite and React logos to learn more</p>
